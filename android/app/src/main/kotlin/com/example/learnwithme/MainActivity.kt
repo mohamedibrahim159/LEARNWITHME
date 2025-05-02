73430ab7ce2c1d291cb043463a21d9d80fbeecc7
Homossssssa <mohamed7ibrahim159@gmail.com>
2025-05-02 05:13:42 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/learnwithme/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learnwithme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.amber
+        ),
+      ),
+    );
+  }
+}
